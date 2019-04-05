beea560cf21e6c5907569f8f8a869a1eca55e2a9
Musa Musa <musabrillz@gmail.com>
2019-04-05 01:47:48 +0100

    GET /pizzas route

--- api.js
+++ api.js
@@ -1,14 +1,22 @@
 'use strict'
 const Api = require('claudia-api-builder');
 const api = new Api();
+const getPizzas = require('./handlers/get-pizzas');
+
+api.get('/', ()=> 'Welcome to Pizza API');
 
 api.get('/pizzas', ()=>{
-    return [
-        'Capricciosa',
-        'Quattro Formaggi',
-        'Napoletana',
-        'Margherita'
-    ]
+    return getPizzas();
 });
+
+api.get('/pizzas/{id}', (request) =>{
+    return getPizzas(request.pathParams.id);
+}, {
+    error: 404
+});
+
+module.exports = api;
+
+
 
 module.exports = api;
